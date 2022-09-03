4cf79e70d678a867b70410e26359699555292be9
Eran Geva <eran.geva@gmail.com>
2022-09-03 13:45:23 +0300

    use torus knot

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 import * as THREE from 'three';
 import { RoomEnvironment } from './node_modules/three/examples/jsm/environments/RoomEnvironment.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GUI } from './node_modules/three/examples/jsm/libs/lil-gui.module.min.js';
+import { Dcel } from './Dcel.js';
+import * as BufferGeometryUtils from './node_modules/three/examples/jsm/utils/BufferGeometryUtils.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(2.5, 5, 35);
+class App {
+    go() {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.set(2.5, 5, 35);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls(camera, renderer.domElement);
-scene.environment = new THREE.PMREMGenerator(renderer).fromScene(new RoomEnvironment()).texture;
-scene.background = new THREE.Color(0x888888);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+        const controls = new OrbitControls(camera, renderer.domElement);
+        scene.environment = new THREE.PMREMGenerator(renderer).fromScene(new RoomEnvironment()).texture;
+        scene.background = new THREE.Color(0x888888);
 
-const gui = new GUI();
+        const mesh = new THREE.Mesh(new THREE.TorusKnotGeometry(10, 2), new THREE.MeshStandardMaterial({ color: 'green' }));
+        mesh.geometry = BufferGeometryUtils.mergeVertices(mesh.geometry);
+        scene.add(mesh);
 
-const params = {
-    numberOfPoints: 5,
-    wireframe: true,
-};
+        const dcel = new Dcel(mesh.geometry);
 
-gui.add(params, 'numberOfPoints').min(2).max(10).step(1).onChange(() => {});
-gui.add(params, 'wireframe').onChange(() => {});
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        };
 
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-};
+        animate();
 
-animate();
+        return this;
+    }
+}
+
+window.app = new App().go();
