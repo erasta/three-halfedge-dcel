0db1773c3881a4c4412b561baf49c04e1df02a1a
Eran Geva <eran.geva@gmail.com>
2025-03-08 13:58:43 +0000

    format

--- demo/app.js
+++ demo/app.js
@@ -1,113 +1,113 @@
-import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import {
-    Color,
-    Float32BufferAttribute,
-    Mesh,
-    MeshStandardMaterial,
-    PerspectiveCamera,
-    PMREMGenerator,
-    Raycaster,
-    Scene,
-    TorusKnotGeometry,
-    Vector2,
-    WebGLRenderer,
+  Color,
+  Float32BufferAttribute,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  PMREMGenerator,
+  Raycaster,
+  Scene,
+  TorusKnotGeometry,
+  Vector2,
+  WebGLRenderer,
 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
 import { Dcel } from '../dist/Dcel/Dcel.js';
 
 class App {
-    go() {
-        const scene = new Scene();
-        const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.set(2.5, 5, 35);
+  go() {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(2.5, 5, 35);
 
-        const renderer = new WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-        const controls = new OrbitControls(camera, renderer.domElement);
-        scene.environment = new PMREMGenerator(renderer).fromScene(new RoomEnvironment()).texture;
-        scene.background = new Color(0x888888);
+    const renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    scene.environment = new PMREMGenerator(renderer).fromScene(new RoomEnvironment()).texture;
+    scene.background = new Color(0x888888);
 
-        const geometry = new TorusKnotGeometry(10, 2, 200, 32, 3, 5);
-        this.mesh = new Mesh(geometry, new MeshStandardMaterial({ vertexColors: true }));
-        this.defaultColor = new Color('green');
-        this.mesh.geometry.setAttribute('color', new Float32BufferAttribute(this.mesh.geometry.attributes.position.array, 3));
-        for (let f = 0, fl = this.mesh.geometry.index.count / 3; f < fl; f++) {
-            this.setFaceColor(f, this.defaultColor);
-        }
-        scene.add(this.mesh);
+    const geometry = new TorusKnotGeometry(10, 2, 200, 32, 3, 5);
+    this.mesh = new Mesh(geometry, new MeshStandardMaterial({ vertexColors: true }));
+    this.defaultColor = new Color('green');
+    this.mesh.geometry.setAttribute('color', new Float32BufferAttribute(this.mesh.geometry.attributes.position.array, 3));
+    for (let f = 0, fl = this.mesh.geometry.index.count / 3; f < fl; f++) {
+      this.setFaceColor(f, this.defaultColor);
+    }
+    scene.add(this.mesh);
 
-        const start = Date.now();
-        this.dcel = new Dcel(geometry);
-        console.log('build dcel took:', Date.now() - start, 'ms for ', this.dcel.faces.length, 'faces');
+    const start = Date.now();
+    this.dcel = new Dcel(geometry);
+    console.log('build dcel took:', Date.now() - start, 'ms for ', this.dcel.faces.length, 'faces');
 
-        this.colorForLevel = Array.from({ length: 20 }).map((_, i, arr) => new Color().setHSL(i / arr.length, 1, 0.5));
-        this.facesIncluded = this.dcel.faces.map(_ => false);
-        this.facesForLevel = this.colorForLevel.map(_ => []);
+    this.colorForLevel = Array.from({ length: 20 }).map((_, i, arr) => new Color().setHSL(i / arr.length, 1, 0.5));
+    this.facesIncluded = this.dcel.faces.map(_ => false);
+    this.facesForLevel = this.colorForLevel.map(_ => []);
 
-        const raycaster = new Raycaster();
-        const pointer = new Vector2();
-        window.addEventListener('pointermove', (event) => {
-            pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-            pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
-            raycaster.setFromCamera(pointer, camera);
-            const inter = raycaster.intersectObject(this.mesh);
-            this.calcColorsByIntersection(inter);
+    const raycaster = new Raycaster();
+    const pointer = new Vector2();
+    window.addEventListener('pointermove', (event) => {
+      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+      pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+      raycaster.setFromCamera(pointer, camera);
+      const inter = raycaster.intersectObject(this.mesh);
+      this.calcColorsByIntersection(inter);
+    });
+
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return this;
+  }
+
+  calcColorsByIntersection(inter) {
+    // reset previous colors to default
+    for (let i = 0; i < this.facesForLevel.length; ++i) {
+      for (const faceIndex of this.facesForLevel[i]) {
+        this.setFaceColor(faceIndex, this.defaultColor);
+      }
+      this.facesForLevel[i] = [];
+    }
+
+    if (inter.length) {
+      const faceIndex = inter[0].faceIndex;
+      this.facesIncluded.fill(false);
+      this.facesForLevel[0].push(faceIndex);
+      this.facesIncluded[faceIndex] = true;
+
+      for (let i = 1; i < this.facesForLevel.length; ++i) {
+        this.facesForLevel[i - 1].forEach((faceIndex) => {
+          this.dcel.forAdjacentFaces(faceIndex, adjFaceIndex => {
+            if (!this.facesIncluded[adjFaceIndex]) {
+              this.facesForLevel[i].push(adjFaceIndex);
+              this.facesIncluded[adjFaceIndex] = true;
+            }
+          });
         });
+      }
 
-        function animate() {
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
-        };
-
-        animate();
-
-        return this;
+      // Color faces by levels, a.k.a distance from intersection point
+      for (let i = 0; i < this.facesForLevel.length; ++i) {
+        for (const faceIndex of this.facesForLevel[i]) {
+          this.setFaceColor(faceIndex, this.colorForLevel[i]);
+        }
+      }
     }
 
-    calcColorsByIntersection(inter) {
-        // reset previous colors to default
-        for (let i = 0; i < this.facesForLevel.length; ++i) {
-            for (const faceIndex of this.facesForLevel[i]) {
-                this.setFaceColor(faceIndex, this.defaultColor);
-            }
-            this.facesForLevel[i] = [];
-        }
+    this.mesh.geometry.attributes.color.needsUpdate = true;
+  }
 
-        if (inter.length) {
-            const faceIndex = inter[0].faceIndex;
-            this.facesIncluded.fill(false);
-            this.facesForLevel[0].push(faceIndex);
-            this.facesIncluded[faceIndex] = true;
-
-            for (let i = 1; i < this.facesForLevel.length; ++i) {
-                this.facesForLevel[i - 1].forEach((faceIndex) => {
-                    this.dcel.forAdjacentFaces(faceIndex, adjFaceIndex => {
-                        if (!this.facesIncluded[adjFaceIndex]) {
-                            this.facesForLevel[i].push(adjFaceIndex);
-                            this.facesIncluded[adjFaceIndex] = true;
-                        }
-                    });
-                });
-            }
-
-            // Color faces by levels, a.k.a distance from intersection point
-            for (let i = 0; i < this.facesForLevel.length; ++i) {
-                for (const faceIndex of this.facesForLevel[i]) {
-                    this.setFaceColor(faceIndex, this.colorForLevel[i]);
-                }
-            }
-        }
-
-        this.mesh.geometry.attributes.color.needsUpdate = true;
+  setFaceColor(faceIndex, color) {
+    for (let f = faceIndex * 3, fl = f + 3; f < fl; f++) {
+      const vertexIndex = this.mesh.geometry.index.array[f];
+      color.toArray(this.mesh.geometry.attributes.color.array, vertexIndex * 3);
     }
-
-    setFaceColor(faceIndex, color) {
-        for (let f = faceIndex * 3, fl = f + 3; f < fl; f++) {
-            const vertexIndex = this.mesh.geometry.index.array[f];
-            color.toArray(this.mesh.geometry.attributes.color.array, vertexIndex * 3);
-        }
-    }
+  }
 }
 
 new App().go();
--- src/Dcel/Dcel.ts
+++ src/Dcel/Dcel.ts
@@ -11,79 +11,79 @@ type DcelFaceNode = Face & { index: number };
  */
 
 export class Dcel {
-    readonly vertices: DcelVertexNode[];
-    readonly faces: DcelFaceNode[];
+  readonly vertices: DcelVertexNode[];
+  readonly faces: DcelFaceNode[];
 
-    constructor(
-        geometry: BufferGeometry,
-        options?: { mergeVerticesThreshold: number; },
-    ) {
-        const num = geometry.attributes.position?.count || 0;
-        this.vertices = Array.from({ length: num }, (_, i) => {
-            const point = new Vector3().fromBufferAttribute(geometry.attributes.position!, i);
-            const node = new VertexNode(point) as DcelVertexNode;
-            node.index = i
-            return node;
-        });
+  constructor(
+    geometry: BufferGeometry,
+    options?: { mergeVerticesThreshold: number; },
+  ) {
+    const num = geometry.attributes.position?.count || 0;
+    this.vertices = Array.from({ length: num }, (_, i) => {
+      const point = new Vector3().fromBufferAttribute(geometry.attributes.position!, i);
+      const node = new VertexNode(point) as DcelVertexNode;
+      node.index = i
+      return node;
+    });
 
-        const threshold = !options ? 1e-4 : options.mergeVerticesThreshold;
-        if (threshold) {
-            const hashToVertex: { [key: string]: number } = {}
-            this.vertices.forEach(v => {
-                const hash = `${~~(v.point.x / threshold)},${~~(v.point.y / threshold)},${~~(v.point.z / threshold)}`;
-                if (hash in hashToVertex) {
-                    v.index = hashToVertex[hash]!;
-                } else {
-                    hashToVertex[hash] = v.index;
-                }
-            });
+    const threshold = !options ? 1e-4 : options.mergeVerticesThreshold;
+    if (threshold) {
+      const hashToVertex: { [key: string]: number } = {}
+      this.vertices.forEach(v => {
+        const hash = `${~~(v.point.x / threshold)},${~~(v.point.y / threshold)},${~~(v.point.z / threshold)}`;
+        if (hash in hashToVertex) {
+          v.index = hashToVertex[hash]!;
+        } else {
+          hashToVertex[hash] = v.index;
         }
-
-        const faceIndices = new Vector3();
-        this.faces = Array.from({ length: geometry.index!.count / 3 }, (_, i) => {
-            faceIndices.fromArray(geometry.index!.array, i * 3);
-            const a = this.vertices[faceIndices.x]!;
-            const b = this.vertices[faceIndices.y]!;
-            const c = this.vertices[faceIndices.z]!;
-            const face = Face.create(a, b, c) as DcelFaceNode;
-            face.index = i;
-            return face;
-        });
-
-        const hashToEdge = new Map();
-        this.faces.forEach(face => {
-            this.forEdges(face, e => {
-                if (!e.twin) {
-                    const hashInv = (e.tail() as DcelVertexNode).index * num + (e.head() as DcelVertexNode).index;
-                    const other = hashToEdge.get(hashInv);
-                    if (other) {
-                        e.setTwin(other);
-                    } else {
-                        const hash = (e.head() as DcelVertexNode).index * num + (e.tail() as DcelVertexNode).index;
-                        hashToEdge.set(hash, e);
-                    }
-                }
-            });
-        });
-
+      });
     }
 
-    forEdges(face: DcelFaceNode, callback: (edge: HalfEdge, face: DcelFaceNode) => void) {
-        const start = face.edge;
-        let e = start;
-        while (true) {
-            callback(e, face);
-            e = e.next;
-            if (e === start) {
-                break;
-            }
+    const faceIndices = new Vector3();
+    this.faces = Array.from({ length: geometry.index!.count / 3 }, (_, i) => {
+      faceIndices.fromArray(geometry.index!.array, i * 3);
+      const a = this.vertices[faceIndices.x]!;
+      const b = this.vertices[faceIndices.y]!;
+      const c = this.vertices[faceIndices.z]!;
+      const face = Face.create(a, b, c) as DcelFaceNode;
+      face.index = i;
+      return face;
+    });
+
+    const hashToEdge = new Map();
+    this.faces.forEach(face => {
+      this.forEdges(face, e => {
+        if (!e.twin) {
+          const hashInv = (e.tail() as DcelVertexNode).index * num + (e.head() as DcelVertexNode).index;
+          const other = hashToEdge.get(hashInv);
+          if (other) {
+            e.setTwin(other);
+          } else {
+            const hash = (e.head() as DcelVertexNode).index * num + (e.tail() as DcelVertexNode).index;
+            hashToEdge.set(hash, e);
+          }
         }
-    }
+      });
+    });
 
-    forAdjacentFaces(faceIndex: number, callback: (adjFaceIndex: number) => void) {
-        this.forEdges(this.faces[faceIndex]!, e => {
-            callback((e.twin.face as DcelFaceNode).index);
-        });
+  }
+
+  forEdges(face: DcelFaceNode, callback: (edge: HalfEdge, face: DcelFaceNode) => void) {
+    const start = face.edge;
+    let e = start;
+    while (true) {
+      callback(e, face);
+      e = e.next;
+      if (e === start) {
+        break;
+      }
     }
+  }
+
+  forAdjacentFaces(faceIndex: number, callback: (adjFaceIndex: number) => void) {
+    this.forEdges(this.faces[faceIndex]!, e => {
+      callback((e.twin.face as DcelFaceNode).index);
+    });
+  }
 }
 
